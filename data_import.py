b24e3b5ac9c5a473ea84afb7fb3ee1b101b6c80c
xudi <xudi1989@ruc.edu.cn>
2017-02-17 10:34:01 +0800

    all right

--- data_import.py
+++ data_import.py
@@ -44,34 +44,6 @@ def import_one_month(month,root_path,start_date):
     def timeSplit(time_stamp):
         [hh,mm],[ss,mili] = time_stamp.split(':')[:2],time_stamp.split(':')[-1].split('.')
         return int(hh),int(mm),int(ss),int(mili)
-    
-    def fill_open(df):
-        
-        #find first -1
-        first_valid = -1
-        for i in xrange(len(df)):
-            if df.iloc[i]['spot'] != -1:
-                break
-            else:
-                first_valid = i
-                break
-        #found!    
-        if first_valid != -1:
-            has_open = True
-            for i in xrange(len(df)):
-                if df.iloc[i]['spot'] > 0:
-                    has_open = False
-                    break
-                elif df.iloc[i]['spot'] == 0:
-                    has_open = True
-                    break
-            #no exist open , so fill open directly
-            if not has_open:
-                df.ix[first_valid,'spot'] = 0
-            else:
-                df.drop(df.index[first_valid],axis = 0)
-                
-        return first_valid
                 
     for idir in dirs:
         infiles = os.listdir(idir)
@@ -86,32 +58,43 @@ def import_one_month(month,root_path,start_date):
         for ins,infile in zip(inss,infiles):
             print ins
             df = pd.read_csv(os.path.join(idir,infile),index_col = None,usecols = [0,1,3,4,5,6,7,8],parse_dates = False)
+            
             df['spot'] = df['Time'].apply(timeSplit).apply(fspot.fcffex_time2spot)
             df['day'] = pd.Series([day] * len(df),index = df.index)
             df['id'] = pd.Series([infile.split('.')[0]] * len(df),index = df.index)
-#             df.sort_values(by = 'spot', axis = 1, inplace = True)
-            first_valid = fill_open(df)
-            df.index = df['spot']
-            df.drop('spot',axis = 1,inplace = True )
-            df = df[ df.index != -1 ]
+            
             print df.head()
+            
+            df.sort_values(by = 'spot', axis = 0, inplace = True)
+            df.drop_duplicates('spot', keep = 'first', inplace = True)
+            df.index = df['spot']
+            df = df[ df.index != -1 ]
+            
+            df.drop('spot',axis = 1,inplace = True )
+            first_one = df.index[0]
             df = df.reindex(xrange(fspot.cffex_last),method = 'pad')
+            
             df.fillna(method = 'pad',inplace = True)
-            if first_valid >= 0:
-                df.iloc[:first_valid+1].fillna(method = 'backfill',inplace = True)
+            if first_one > 0:
+                df.ix[:1,:].fillna(method = 'backfill',inplace = True)
+            print df.head()
+            
             df['spot'] = df.index
             df['day'] = df['day'].apply(np.int)
             df['TradeVolume'] = df['TradeVolume'].apply(np.int)
-            print df.head()
-#             print df.tail()
+            
+#             print df.head()
+           
 #             start = time.time()
 #             new_records.insert_data_frame(new_records.if_struct, df, merge = False)
             df.to_sql(str(day),new_records.engine,index = False,if_exists = 'append',chunksize = 2048) 
 #             end = time.time()
 #             print 'elapsed = ', end - start
+
+            
     
 if __name__ == '__main__':
-    month = 201403
-    start_date = 20140305
-    import_path = r'D:\future\data\CFFEX\CFFEX_201403\CFFEX\201403\IF'
+    month = 201401
+    start_date = 0
+    import_path = r'D:\future\data\CFFEX\CFFEX_201401\201401\IF'
     import_one_month(month,import_path,start_date) 
