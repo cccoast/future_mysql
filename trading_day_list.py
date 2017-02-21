ef894ded6f3b6d3c5410f2abbea153162838cbdb
xudi <xudi1989@ruc.edu.cn>
2017-02-21 12:40:14 +0800

    add trading day list

---
+++ trading_day_list.py
@@ -0,0 +1,30 @@
+import dbBase as db
+
+from sqlalchemy import Column, Integer
+from sqlalchemy import Table
+
+import pandas as pd
+
+class Dates(db.DB_BASE):
+    
+    def __init__(self):
+        db_name,table_name = 'dates','trading_days'
+        super(Dates,self).__init__(db_name)
+        
+        self.table_struct = Table(table_name,self.meta,
+                     Column('date',Integer,primary_key = True,autoincrement = False),
+                    )
+        
+    def create_table(self):
+        self.date_struct = self.quick_map(self.table_struct)
+        
+if __name__ == '__main__':
+    
+    infile = r'D:\future\dates.csv'
+    df = pd.read_csv(infile,parse_dates = [0])
+    df['date'] = df['date'].apply(lambda x: int(x.year * 10000 + x.month * 100 + x.day))
+    print df.head()
+    dates = Dates()
+    dates.create_table()
+    df.to_sql('trading_days',dates.engine,index = False,if_exists = 'append',chunksize = 2048) 
+    
