9c1b1793f6213c85a19b7a20acf41dc7a949f837
xudi <xudi1989@ruc.edu.cn>
2017-06-12 18:02:01 +0800

    finish cffex future rolling rule

--- misc.py
+++ misc.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 timestamp2int = lambda x: x.year * 10000 + x.month * 100 + x.day
+get_year_month_day = lambda x: (int(x/10000),int((x%10000)/100),int(x%100))
 
 def get_special_weekday(indate):
     if not isinstance(indate,pd.tslib.Timestamp):
@@ -21,8 +22,11 @@ def get_specical_weekday_in_daterange(start_day,end_day,weekday):
 
 def get_nth_specical_weekday_in_daterange(start_day,end_day,weekday,nth):
     dates = get_specical_weekday_in_daterange(start_day,end_day,weekday)
-    return [ timestamp2int(month_nth.index[nth]) for month,month_nth in dates.groupby(by = lambda x: x.month) ]
-    
+    ret = {}
+    for year,year_dates in dates.groupby(lambda x:x.year):
+        ret[year] = [ timestamp2int(month_nth.index[nth]) for month,month_nth in year_dates.groupby(by = lambda x: x.month) ]
+    return ret
+
 if __name__ == '__main__':
     print get_nth_specical_weekday_in_daterange(20140101,20141231,5,0)
     
--- trading_day_list.py
+++ trading_day_list.py
@@ -1,12 +1,14 @@
 import dbBase as db
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, FLOAT
 from sqlalchemy import Table
 
 import pandas as pd
+import numpy as np
 from table_struct import cffex_if,cffex_if_min
+from itertools import chain
 
-from misc import get_nth_specical_weekday_in_daterange,timestamp2int
+from misc import get_nth_specical_weekday_in_daterange,timestamp2int,get_year_month_day
 
 class Dates(db.DB_BASE):
     
@@ -48,10 +50,10 @@ class Dates(db.DB_BASE):
 
 class futureOrder(db.DB_BASE):
     
-    def __init__(self,ticker):
-        db_name,table_name = 'dates','future_order' + '_' + str.lower(ticker[:2])
+    def __init__(self,market_id):
+        db_name,table_name = 'dates','future_order' + '_' + str.lower(market_id[:2])
         super(futureOrder,self).__init__(db_name)
-        
+        self.table_name = table_name
         self.table_struct = Table(table_name,self.meta,
                      Column('date',Integer,primary_key = True,autoincrement = False),
                      Column('if0001',String(20)),
@@ -62,43 +64,45 @@ class futureOrder(db.DB_BASE):
         
         self.future_order_struct = self.quick_map(self.table_struct)
     
-    def get_exchange_rollling_day_if(self):
+    def get_exchange_rollling_day_if(self,offset = 0):
         weekday,nth = 5,2
         dates = Dates()
         trading_days = dates.get_trading_day_list()
         last_trading_day = timestamp2int( pd.to_datetime(str(trading_days[-1])) + pd.to_timedelta(1,unit = 'D'))
-        third_fridays = get_nth_specical_weekday_in_daterange(trading_days[0], last_trading_day, weekday, nth)
-        exchange_rolling_days = map(lambda x: dates.get_first_less_than(x),third_fridays)
-        return exchange_rolling_days
+        print 'first day = ',trading_days[0],' last_trading_day = ',last_trading_day
+        weekdays = [ i for i in chain(*get_nth_specical_weekday_in_daterange(trading_days[0], last_trading_day, weekday, nth).values())]
+        exchange_rolling_days = map(lambda x: dates.get_first_less_than(x),weekdays)
+        trading_days_series = pd.Series(index = trading_days,data = range(len(trading_days)))
+        roll_days_dict =  { year:{} for year in  np.unique( map(lambda x: int(x/10000), exchange_rolling_days) ) }
+        for day in exchange_rolling_days:
+            year = int(day/10000)
+            roll_days_dict[year][ int((day%10000)/100) ] = trading_days[ trading_days_series[ day ] - offset ] \
+                                                            if trading_days_series[ day ] - offset >= 0 else day
+        return roll_days_dict
         
-    def set_order_cffex_if(self):
-        
+    def set_order_cffex_if(self,method = 'fixed_days',fixed_days = 5):
         #use min tables instead of tick tables to accelerate!
         dbname = 'cffex_if_min'
-        
         dates = Dates()
-        ss = dates.session()
-        records = ss.query(dates.date_struct).all()
-        for irec in records:
-            date = irec.date
+        rolling_day = self.get_exchange_rollling_day_if(fixed_days)
+        exchang_rolling_day = self.get_exchange_rollling_day_if(0)
+        trading_day_list = dates.get_trading_day_list()
+        for date in trading_day_list:
             if_table_obj = cffex_if_min(db_name=dbname,table_name=str(date))
             if not if_table_obj.check_table_exist():
                 continue 
             print date
-                
-            min_table = pd.read_sql_table(str(date), if_table_obj.engine)
-            tickers = min_table['id'].unique()
-            
-            
-        
-            sql = 'select distinct id from cffex_if_min.{0} order by id;'.format(str(date))
-            tickers = if_table_obj.execute_sql(sql)
-            orders = [ irec[0] for irec in tickers ]
-            to_be_inserted = [date,]
-            to_be_inserted.extend(orders)
-            print to_be_inserted
-            self.insert_listlike(self.future_order_struct, to_be_inserted, True)
-        ss.close()
+            year,month,day = get_year_month_day(date)
+            if method == 'fixed_days':
+                sql = 'select distinct id from cffex_if_min.{0} order by id;'.format(str(date))
+                tickers = if_table_obj.execute_sql(sql)
+                orders = [ irec[0] for irec in tickers ]
+                if day > rolling_day[year][month] and day <= exchang_rolling_day[year][month]:
+                    orders[0],orders[1] = orders[1],orders[0]
+                to_be_inserted = [date,]
+                to_be_inserted.extend(orders)
+                print to_be_inserted
+                self.insert_listlike(self.future_order_struct, to_be_inserted, True)
         
 def import_trading_days():
     
@@ -134,11 +138,7 @@ def adjust_days():
     set_future_order_if()
     
 if __name__ == '__main__':
-    
-    dates = Dates()
-    print dates.get_trading_day_list()
-    
     order = futureOrder('if')
-    print order.get_exchange_rollling_day_if()
+    print order.get_exchange_rollling_day_if(offset = 5)
     
     
