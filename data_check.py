8e26a688b1745934619794a64f800a8cac7b7b6a
xudi <xudi1989@ruc.edu.cn>
2017-06-13 15:01:00 +0800

    adjustor data check

--- data_check.py
+++ data_check.py
@@ -65,10 +65,35 @@ def get_nostruct_data_from_memory(ipckey,nostruct_ipckey):
     
     print milli_sec_list
     print spots_list
-        
+
+def check_adjust(ipckey1,ipckey2):
+    
+    no_adjust_api = ShmPython.Shm(ipckey1)
+    adjusted_api  = ShmPython.Shm(ipckey2)
+    header = no_adjust_api.getHeader()
+    trading_day_list = no_adjust_api.getTradingDayList()
+    trading_day_map  = pd.Series(index = trading_day_list,data = range(len(trading_day_list)))
+    spots_count_perday = header.getSpotsCountPerDay()
+    from misc import get_nth_specical_weekday_in_daterange
+    from itertools import chain
+    weekdays = [i for i in chain(*(get_nth_specical_weekday_in_daterange(20140101,20151231,5,1).values()))]
+    first_roll_day = weekdays[-1]
+    nth_first_roll_day = trading_day_map[first_roll_day]
+    ind_index,ins_index = 0,0
+    datas1 = no_adjust_api.fetchDoubleDataList(ind_index,ins_index,0,spots_count_perday * nth_first_roll_day)
+    datas2 = adjusted_api.fetchDoubleDataList(ind_index,ins_index,0,spots_count_perday * nth_first_roll_day)
+    print 'cum adjust value = ',(datas2[0] - datas1[0])
+    plt.plot(datas1,label = 'no_adjust')
+    plt.plot(datas2,label = 'adjusted')
+    plt.legend()
+    plt.show()
+    #pdiff.txt comes from what adjustor output, line 74
+    df = pd.read_csv('~/tmp/pdiff{}.txt'.format(ins_index + 1),index_col = 0,header = None,sep = ' ')
+    print df[5].sum()
+    
 if __name__ == '__main__':
 #     get_table_from_sql_db('cffex_if','20150119')
 #     get_data_from_memory('0x0f0f0005')
 #     get_nostruct_data_from_memory('0x0f0f0002','0x0e0e0002')
-    check_trading_day_list('0x0f0f0002')
-    
+#     check_trading_day_list('0x0f0f0002')
+    check_adjust('0x0f0f0002','0x0f0f0004')
